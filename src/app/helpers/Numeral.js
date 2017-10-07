65d586f5e088aa0352d04bf228f2cc14d661ca4b
Allan Ramos <allangrds@gmail.com>
2017-10-07 16:17:37 -0300

    Simplificando componente Numeral

--- src/app/helpers/Numeral.js
+++ src/app/helpers/Numeral.js
@@ -1,20 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import numeral from 'numeral';
 
-export default class Numeral extends Component {
-  constructor() {
-    super();
-    numeral.locale('pt-br', require('numeral/locales/pt-br'));
-  }
+numeral.locale('pt-br', require('numeral/locales/pt-br'));
 
-  render() {
-    const { format, value } = this.props;
-
-    return <span>{numeral(value).format(format)}</span>;
-  }
-}
+const Numeral = ({ format, value }) => {
+  return <span>{numeral(value).format(format)}</span>;
+};
 
 Numeral.propTypes = {
   format: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired
 };
+
+export default Numeral;
