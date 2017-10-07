6151fd7d0ae402af004df7c8f0ea3f4b1c810a28
Allan Ramos <allangrds@gmail.com>
2017-10-07 14:06:48 -0300

    * Excluindo lodash do package.json; * Enxugando http.js, pois só o uso o método GET;

--- src/app/helpers/http.js
+++ src/app/helpers/http.js
@@ -1,43 +1,18 @@
-import { assign } from 'lodash';
-
-require('whatwg-fetch');
-
-const acceptJson = { Accept: 'application/json; charset=UTF-8' };
-const sendsJson = { 'Content-Type': 'application/json; charset=UTF-8' };
-
 const apiFetch = (endpoint, config = {}) => {
-  const reqConfig = assign({}, config);
-
-  reqConfig.headers = assign(reqConfig.headers || {}, acceptJson);
-
-  if (reqConfig.body) {
-    reqConfig.body = JSON.stringify(reqConfig.body);
-    assign(reqConfig.headers, sendsJson);
-  }
-
-  if (reqConfig.multipart) {
-    reqConfig.body = reqConfig.multipart;
-  }
-
-  return fetch(endpoint, reqConfig)
+  return fetch(endpoint, config)
     .then(checkStatus)
     .then(toJSON);
 };
 
 const checkStatus = res => (res.ok ? res : Promise.reject(res));
 
-const toJSON = res =>
-  res.status !== 202 && res.status !== 204 && res.json ? res.json() : res;
+const toJSON = res => (res.status === 200 && res.json ? res.json() : res);
 
 const useMethod = methodName => (endpoint, config) =>
-  apiFetch(endpoint, assign({ method: methodName }, config));
+  apiFetch(endpoint, Object.assign({ method: methodName }, config));
 
-const GET = apiFetch;
-const DELETE = useMethod('DELETE');
-const POST = useMethod('POST');
-const PUT = useMethod('PUT');
-const PATCH = useMethod('PATCH');
+const GET = useMethod('GET');
 
-export { GET, DELETE, POST, PUT, PATCH };
+export { GET };
 
 export default apiFetch;
